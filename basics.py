3196b8f879cd603d68f65202d3f02ed121d5845a
Christina Windall <christinahelen@hotmail.co.uk>
2021-11-15 11:25:40 +0000

    Just revising the basics

---
+++ basics.py
@@ -0,0 +1,8 @@
+name=input("Please enter your name:")
+surname=input("Please enter your surname:")
+print("Hello, "+name+" "+surname)
+
+number1=float(input('Please enter a number:'))
+number2=float(input('Please enter another number:'))
+ans=number1+number2
+print(number1,"+",number2,"=",ans)

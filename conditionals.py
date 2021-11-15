197efd79061f9ffb495f9f3633ccf2d6057d44c6
Christina Windall <christinahelen@hotmail.co.uk>
2021-11-15 18:06:29 +0000

    Revising vonditionals

---
+++ conditionals.py
@@ -0,0 +1,46 @@
+#devs_money=9
+#dev_plays=False
+#if devs_money>10 and dev_plays:
+#    print('Dev can play')
+#elif devs_money<10 and dev_plays:
+#    print('Dev can\'t afford to play')
+#else:
+#    print('Dev can\'t play')
+
+#mark=input('User mark:')
+#nummark=float(mark)
+#if nummark<=100:
+#    if nummark>85:
+#        print('Distinction')
+#    elif 65<nummark<=85:
+#        print('Pass')
+#    else:
+#        print('Fail')
+#else:
+#    print('Please enter a valid mark')
+
+#num=int(input('Please enter a natural number:'))
+#if num%2==0:
+#    print('Even')
+#else:
+#    print('Odd')
+
+#hum_age=float(input('Please enter your age:'))
+#if hum_age<2:
+#    dog_age=hum_age*5.25
+#    print('Your dog age is',dog_age)
+#else:
+#    dog_age=10.5+(hum_age-2)*7
+#    print('Your dog age is',dog_age)
+
+#vowels=('a','e','i','o','u')
+#letter=input('Please enter a letter:')
+#if letter.isalpha():
+#    if letter.lower() in vowels:
+#        print('Vowel')
+#    else:
+#        print('Consonant')
+#else:
+#    print('This is not a letter.')
+
+

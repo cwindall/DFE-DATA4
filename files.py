669cb04db053f7f6edc81edec9d59dc66af41324
Christina Windall <christinahelen@hotmail.co.uk>
2021-11-18 15:55:43 +0000

    Dealing with files in python

---
+++ files.py
@@ -0,0 +1,24 @@
+# file=open('filename.txt','r')
+# outfile=''
+# for line in range(1,10):
+#     if line%2==0:
+#         outfile+=file.readline()
+#     else:
+#         file.readline()
+
+# file=open('filename.txt','w')
+# file.write(outfile)
+# file.close()
+
+# file=open('teams.txt')
+# lines=file.readlines()
+# print(lines[0],lines[3])
+# lines.remove(lines[0])
+# lines.insert(0,'This is a new line\n')
+# text=''
+# for i in lines:
+#     text+=i
+
+# file=open('teams.txt','w')
+# file.write(text)
+# file.close()
